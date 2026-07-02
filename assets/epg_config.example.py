8ff613b294ad91dd0f76aa41d5007aa127e69cac
weilmy <bachmann.willy@bluewin.ch>
2026-07-02 21:56:55 +0200

    Add epg_config.example.py, ensure real config stays untracked

---
+++ assets/epg_config.example.py
@@ -0,0 +1,8 @@
+# epg_config.example.py
+
+EPG_CLIENT_ID = "..."  # Ihre Consumer Key
+EPG_CLIENT_SECRET = "..."        # Ihr Consumer Secret
+
+# ── https://opentransportdata.swiss/de/ ─────────────────────────────────────────────────────────────
+API_TOKEN = "..."
+API_URL   = "https://api.opentransportdata.swiss/TDP/Soap_Datex2/TrafficSituations/Pull"
